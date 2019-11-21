d9f84ff13b8c39ff9ddfb2f5d10337ab9545c772
bigro <kitkat.jaja@gmail.com>
2019-11-21 14:53:09 +0900

    init project

---
+++ src/main/java/com/example/springaopplaying/SpringAopPlayingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaopplaying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopPlayingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopPlayingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springaopplaying/SpringAopPlayingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaopplaying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopPlayingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
