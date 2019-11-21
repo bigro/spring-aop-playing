d47d9e3fdf22d56011be4e40910b39e1956e59f8
bigro <kitkat.jaja@gmail.com>
2019-11-21 16:30:54 +0900

    @SheduledごとのtraceIdをLogに出力できる

--- src/main/java/com/example/springaopplaying/aop/log/StartEndLog.java
+++ src/main/java/com/example/springaopplaying/aop/log/StartEndLog.java
@@ -4,6 +4,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -11,13 +13,15 @@ import java.time.LocalDateTime;
 @Aspect
 @Component
 public class StartEndLog {
+    private static final Logger logger = LoggerFactory.getLogger(StartEndLog.class);
+
     @Before("execution(* *..*ServiceA.*(..))")
     public void startLog(JoinPoint joinPoint) {
-        System.out.println(joinPoint.getSignature() + " 開始: " + LocalDateTime.now());
+        logger.info("Start {}, time: {}", joinPoint.getSignature(), LocalDateTime.now());
     }
 
     @After("execution(* *..*ServiceA.*(..))")
     public void afterLog(JoinPoint joinPoint) {
-        System.out.println(joinPoint.getSignature() + " 終了: " + LocalDateTime.now());
+        logger.info("End {}, time: {}", joinPoint.getSignature(), LocalDateTime.now());
     }
 }
--- src/main/java/com/example/springaopplaying/service/ServiceA.java
+++ src/main/java/com/example/springaopplaying/service/ServiceA.java
@@ -1,10 +1,12 @@
 package com.example.springaopplaying.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceA {
 
+    @Scheduled(fixedDelay = 5000)
     public void loop() {
         int sum = 0;
         for (int i = 0; i < 100000; i++) {
